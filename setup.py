87f891671ad5d1fbccc36ba7b75c74a1d686e853
Cornelius Roemer <cornelius.roemer@gmail.com>
2021-09-27 01:47:28 +0200

    change: name to new fasta_zstd_sqlite / fzs

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name="store",
-    description="CLI tool to store FASTA files compressed with zstd in sqlite",
+    name="fasta_zstd_sqlite",
+    description="CLI tool to store and retrieve individual zstd compressed FASTA records in sqlite",
     license="MIT",
     author="Cornelius Roemer",
-    url="https://github.com/corneliusroemer/augur_store",
+    url="https://github.com/corneliusroemer/fasta_zstd_sqlite",
     version="0.0.1",
-    py_modules=["augur_store"],
+    py_modules=["fasta_zstd_sqlite"],
     install_requires=[
         "Click>=8",
         "Cloup>=0.11",
@@ -18,7 +18,7 @@ setup(
     python_requires=">=3.7",
     entry_points={
         "console_scripts": [
-            "fz = main:cli",
+            "fzs = main:cli",
         ],
     },
     classifiers=[
